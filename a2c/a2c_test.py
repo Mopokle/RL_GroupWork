08e4defabaab6e787c6e2b739159de1e1d82fe4e
Mopokle <mopokle@gmail.com>
2023-05-03 18:02:41 +0100

    a2c 2000ep

--- a2c/a2c_test.py
+++ a2c/a2c_test.py
@@ -2,6 +2,7 @@ import gym
 import time
 import os
 from a2c_agent import A2CAgent
+from gym.wrappers import Monitor
 
 def test(agent, env, episodes=10, max_steps=1000, render=True, sleep_time=0.01):
     for episode in range(episodes):
@@ -24,13 +25,14 @@ def test(agent, env, episodes=10, max_steps=1000, render=True, sleep_time=0.01):
 
 if __name__ == "__main__":
     env = gym.make("LunarLander-v2")
+    env = Monitor(env, 'videos', force=True)  # Set the output directory for the videos
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = A2CAgent(state_size, action_size)
 
     # Load the trained model from the model folder
     model_dir = "model"
-    model_file = "a2c_lunar_lander_2023-05-03_17-20-00.pth"  # Make sure to use the correct file name for the A3C model
+    model_file = "a2c_lunar_lander_ep1000.pth"  # Make sure to use the correct file name for the A3C model
     model_path = os.path.join(model_dir, model_file)
     agent.load_model(model_path)
 
@@ -38,3 +40,4 @@ if __name__ == "__main__":
     test(agent, env)
 
     env.close()
+
