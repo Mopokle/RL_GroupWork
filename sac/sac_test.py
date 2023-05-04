61c9c7d528f046e9709b42f66e64cdcfddd3666a
Mopokle <mopokle@gmail.com>
2023-05-04 14:25:04 +0100

    sac videos

--- sac/sac_test.py
+++ sac/sac_test.py
@@ -2,8 +2,11 @@ import gym
 import time
 import os
 from sac_agent import SACAgent
+from gym.wrappers import Monitor
 
 def test(agent, env, episodes=10, max_steps=1000, render=True, sleep_time=0.01, video_file=None):
+    if video_file is not None:
+        env = Monitor(env, video_file, force=True, video_callable=lambda episode_id: True)
 
     for episode in range(episodes):
         state = env.reset()
@@ -34,12 +37,14 @@ if __name__ == "__main__":
 
     # Load the trained model from the model folder
     model_dir = "model"
-    model_file = "sac_discrete_lunar_lander_2023-05-04_00-09-09.pth"
+    model_file = "sac_discrete_lunar_lander_2023-05-03_23-21-28.pth"
     model_path = os.path.join(model_dir, model_file)
     agent.load_model(model_path)
 
     # Set the output video file name
-    video_file = "videos"
+    video_dir = "videos"
+    os.makedirs(video_dir, exist_ok=True)  # Create the output video folder if it doesn't exist
+    video_file = os.path.join(video_dir, "sac_lunar_lander_test.mp4")
 
     # Test the model
     test(agent, env, video_file=video_file)
